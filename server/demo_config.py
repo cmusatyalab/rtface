029b02341fb9f954e7a142c4e5138a98ee91f252
Jamesjue <junjuew@cs.cmu.edu>
2016-07-25 01:51:55 +0000

    confirmed meanshift is working. used dlib correlation tracker for better performance. selective detect images for now
    Former-commit-id: e5c5b7399131bcef1ba05a53ecc9bd5df2f9b856

--- server/demo_config.py
+++ server/demo_config.py
@@ -7,10 +7,15 @@ class Config(object):
     MAX_IMAGE_WIDTH=1024
     # dlib tracking takes longer time with a large variations
     # 20ms ~ 100+ ms
-    DLIB_TRACKING=False
+    DLIB_TRACKING=True
     # whether detector should upsample
     # detection with upsample = 1 on a 640x480 image took around 200ms
     # detection with upsample = 0 on a 640x480 image took around 70ms
     DLIB_DETECTOR_UPSAMPLE_TIMES=0
     # adjust face detector threshold, a negative number lower the threshold
     DLIB_DETECTOR_ADJUST_THRESHOLD=-0.5
+
+    # profile face detection
+    DETECT_PROFILE_FACE=True
+    # profile face cascade opencv xml path
+    OPENCV_PROFILE_FACE_CASCADE_PATH='/home/faceswap-admin/dependency/dependency/opencv-src/opencv-3.1.0/data/lbpcascades/lbpcascade_profileface.xml'
