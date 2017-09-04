c8ed66bcbd366d4e2f8d1a3e3e2aec0ef4c5dd5f
Jamesjue <canjian.myself@gmail.com>
2017-09-04 16:52:21 -0400

    add in environment variable templates
    Former-commit-id: 449c155f6ec52df596d3e1ee79064ad8b95cd1b8

---
+++ server/rtfacerc_template.sh
@@ -0,0 +1,9 @@
+"""Set up necessary environment variables
+"""
+
+export GOOGLE_ID='googleid.apps.googleusercontent.com' # Google dev ID
+export GOOGLE_SECRET='super-secret' # Google dev privacy key
+export VIDEO_TRAINER_SECRET='super-secret' # Web trainer secret
+export VIDEO_TRAINER_CERT='keys/trainer.crt' # Trainer SSL cert
+export VIDEO_TRAINER_CERT_SECRET='keys/trainer.key' # Trainer SSL cert key
+export GABRIEL_ROOT='/home/tmp/gabriel-v2' # Path to gabriel
